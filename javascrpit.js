c86048723b5be71209f7b3c0f09a404fc46d99e8
Jesus Montejo <153864613+jesusito3@users.noreply.github.com>
2024-05-10 14:19:49 -0400

    Create table, add new books to library

--- javascrpit.js
+++ javascrpit.js
@@ -15,8 +15,23 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
+const table = document.getElementById("table");
+
 function display() {
     for(i = 0; i < myLibrary.length; i++) {
+        let row = table.insertRow(-1);
+
+        let titleColumn = row.insertCell(0);
+        let authorColumn = row.insertCell(1);
+        let pagesColumn = row.insertCell(2);
+        let readColumn = row.insertCell(3);
+        let readBtnColumn = row.insertCell(4);
+
+        titleColumn.innerHTML = Book.title;
+        authorColumn.innerHTML = Book.author;
+        pagesColumn.innerHTML = Book.pages;
+        readColumn.innerHTML = Book.read;
+
         console.log(myLibrary[i]);
     }
 }
@@ -30,6 +45,11 @@ newBookButton.addEventListener("click", () => {
 });
 
 submitButton.addEventListener("click", (event) => {
+    let titleInput = document.getElementById("title");
+    let authorInput = document.getElementById("author");
+    let pagesInput = document.getElementById("pages");
+    let readInput = document.getElementById("read");
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
     event.preventDefault();
     dialog.close();
 })
